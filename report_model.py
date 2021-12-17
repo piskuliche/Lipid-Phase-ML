b69f5fb85526f406a1604ffbd6a7eafcf1630cca
Zeke Piskulich <piskulichz@gmail.com>
2021-12-17 13:49:11 -0500

    update

--- report_model.py
+++ report_model.py
@@ -2,9 +2,17 @@
 import numpy
 import pickle
 import sys,mllpa
+import subprocess
 
 modelname=str(sys.argv[1])
 
 final_model = pickle.load(open(modelname,'rb'))
 for key in final_model['scores']['final_score']:
     print("%s : %s" % (key, final_model['scores']['final_score'][key]))
+
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+tree.export_graphviz(final_model['ClassificationTree'],out_file="viz.out",feature_names=['SVM-Coords','KNN-Coords','SVM-Distances','NB'],class_names=['gel','fluid'],rounded=True,rotate=True,proportion=True,filled=True)
+
+subprocess.run("dot -Tpng viz.out -o viz.png",shell=True)
